a08ff19262a4bcb8648aae73b42b88fa7123b964
Zafarbek8776 <rajabboyevzafar6@gmail.com>
2025-10-14 14:22:39 +0500

    lambda 1

---
+++ function lambda .py
@@ -0,0 +1,7 @@
+def add(a, b):
+    return a + b
+
+plus = add
+
+result = plus(5, 3)
+print(result)
---
+++ function6.py
@@ -0,0 +1,7 @@
+def safe_divide(a,b):
+ return a / b 
+def error_handler():
+ return "error"
+def run_if_safe(func,handler)
+
+    
