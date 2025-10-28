5ea4266cf356766e93f0b4692c243ab420e1e7ce
Zafarbek8776 <rajabboyevzafar6@gmail.com>
2025-10-28 18:57:26 +0500

    a

---
+++ Decorator/OOP_intro/oop1.py
@@ -0,0 +1,16 @@
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+
+car1 = Car("mersedes", "gelik")
+car2 = Car("chevrolet", "tracker")
+
+
+print("Car 1 Brand:", car1.brand)
+print("Car 1 Model:", car1.model)
+
+print("Car 2 Brand:", car2.brand)
+print("Car 2 Model:", car2.model)
+
