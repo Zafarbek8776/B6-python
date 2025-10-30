419d52a886269a849700a21fd9c012e8b5bfa752
Zafarbek8776 <rajabboyevzafar6@gmail.com>
2025-10-30 20:19:49 +0500

    a

---
+++ Decorator/OOP_intro/Inheritance and Polymorphism/Inheritance 3.py
@@ -0,0 +1,15 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+class Employee(Person):
+    def __init__(self, name, age, salary):
+        super().__init__(name, age)
+        self.salary = salary
+
+emp1 = Employee("Ali", 22, 4000)
+
+print("Name:", emp1.name)
+print("Age:", emp1.age)
+print("Salary:", emp1.salary)
