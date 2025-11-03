573bd4e860c7d2e60608d518dfa595d2a4fe6a6b
Zafarbek8776 <rajabboyevzafar6@gmail.com>
2025-11-03 11:47:12 +0500

    a

---
+++ Decorator/OOP_intro/Inheritance and Polymorphism/Inheritance 27.py
@@ -0,0 +1,19 @@
+class A:
+    def show(self):
+        print("This is from class A")
+
+class B(A):
+    def show(self):
+        print("This is from class B")
+
+class C(A):
+    def show(self):
+        print("This is from class C")
+
+class D(B, C):
+    pass
+
+
+d1 = D()
+
+d1.show()
