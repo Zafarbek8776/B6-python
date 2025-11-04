b1810862ee0afb209267ad0422641f607578f266
Zafarbek8776 <rajabboyevzafar6@gmail.com>
2025-11-04 17:09:23 +0500

    a

---
+++ Decorator/OOP_intro/Polymorphism/Polymorphism 4.py
@@ -0,0 +1,14 @@
+class Car:
+    def drive(self):
+        print("Car is driving on the road")
+
+class Bike:
+    def drive(self):
+        print("Bike is driving in the street")
+
+car1 = Car()
+bike1 = Bike()
+
+
+car1.drive()
+bike1.drive()
